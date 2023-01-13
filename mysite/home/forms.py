72bb4a4dfa982111de1d7104dfdae3e5c42ae014
Tak2009 <50517027+Tak2009@users.noreply.github.com>
2023-01-13 13:13:31 +0000

    changed to the home forms

--- mysite/home/forms.py
+++ mysite/home/forms.py
@@ -41,10 +41,7 @@ from django.core.exceptions import ValidationError
 
 # https://ordinarycoders.com/blog/article/django-user-register-login-logout
 class NewUserForm(UserCreationForm):
-	username = forms.CharField(label='username', min_length=5, max_length=150)
 	email = forms.EmailField(label='email', required=False)
-	password1 = forms.CharField(label='password', widget=forms.PasswordInput)
-	password2 = forms.CharField(label='Confirm password', widget=forms.PasswordInput)
 
 	class Meta:
 		model = User
