1d73a96623465afc4fd185a61009fa45dd948e7b
Tak2009 <50517027+Tak2009@users.noreply.github.com>
2023-01-21 13:45:46 +0000

    wip

--- mysite/home/static/script_github.js
+++ mysite/home/static/script_github.js
@@ -1,4 +1,5 @@
 // https://htmlcolorcodes.com/color-picker/
+// https://qiita.com/7note/items/4d922ebd33dbefe99a89
 background = ['#07FFD2', '#07B0FF', '#0734FF', '#5607FF', '#D207FF', '#FF07B0']
 
 const renderLang = (langs) => {
@@ -10,9 +11,16 @@ const renderLang = (langs) => {
         const div = document.createElement("div")
         div.classList.add("progress")
         div.style.backgroundColor = '#b6bfb6'
+        div.style.height= '30px'
+
         console.log(lang_index, lang)
         ratio = (parseFloat(langs[lang])/sum * 100).toFixed(1)
-        div.innerHTML = `<div class="progress-bar" role="progressbar" style="width: ${ratio}%; background:${background[lang_index % 6]}; color:white" aria-valuenow="${ratio * 100}" aria-valuemin="0" aria-valuemax="100">${lang}(${ratio})%</div>`
+        div.innerHTML = 
+            `<div class="progress-bar" role="progressbar" style="width: ${ratio}%; background:${background[lang_index % 6]}" aria-valuenow="${ratio * 100}" aria-valuemin="0" aria-valuemax="100" onMouseOut="this.style.background='${background[lang_index % 6]}';" onMouseOver="this.style.background='#EEF';">
+                <a href="https://github.com/Tak2009/django-1/search?l=${lang.toLowerCase()}" target="_blank" style="color: #f7f5f5" onMouseOut="this.style.color='#f7f5f5';" onMouseOver="this.style.color='#050505';">
+                    ${lang}(${ratio})%
+                </a>
+            </div>`
         lang_list.appendChild(div)
         lang_index += 1
     }
@@ -30,6 +38,3 @@ API.getLang(django_1_language).then(data => renderLang(data)).catch(console.log)
 //   console.warn(error);
 // });
 
-
-
-// renderLang(langs)
