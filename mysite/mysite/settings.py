852f1ac6b3b87578aa343f95899e01c877cba0da
Tak2009 <50517027+Tak2009@users.noreply.github.com>
2023-01-23 12:16:36 +0000

    change to local db setting

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -119,15 +119,21 @@ try:
             },
         }
     }
-
 except:
+    from . import db_local
     DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'default': {
+            'ENGINE': 'django.db.backends.mysql',
+            'NAME': 'mysql_local_db',
+            'USER': 'root', # default
+            'PASSWORD': db_local.DATABASES['PASSWORD'],
+            'HOST': '127.0.0.1',
+            'PORT': '3306',
+            'OPTIONS': {
+                'init_command': "SET sql_mode='STRICT_TRANS_TABLES'",
+            },
+        }
     }
-}
-
 
 # Password validation
 # https://docs.djangoproject.com/en/3.0/ref/settings/#auth-password-validators
