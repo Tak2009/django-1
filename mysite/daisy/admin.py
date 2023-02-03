9267542ec65b402e1030becd2272895e3b4566f8
Tak2009 <50517027+Tak2009@users.noreply.github.com>
2023-02-03 11:21:29 +0000

    a change made to daisy.model

--- mysite/daisy/admin.py
+++ mysite/daisy/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
 
 # Register your models here.
-from daisy.models import Pic
+from daisy.models import Pic, Comment
 
 admin.site.register(Pic)
+admin.site.register(Comment)
--- mysite/daisy/models.py
+++ mysite/daisy/models.py
@@ -2,8 +2,7 @@ from django.db import models
 
 # Create your models here.
 from django.core.validators import MinLengthValidator
-from django.contrib.auth.models import User
-# from django.conf import settings
+from django.conf import settings #https://learndjango.com/tutorials/django-best-practices-referencing-user-model
 
 class Pic(models.Model) :
     title = models.CharField(
@@ -12,10 +11,27 @@ class Pic(models.Model) :
     )
     # Picture
     pic = models.ImageField(upload_to='', null=True)
-    created_by = models.ForeignKey(User, null=True, blank=True, on_delete=models.CASCADE)
+    comments = models.ManyToManyField(settings.AUTH_USER_MODEL,
+        through='Comment', related_name='comments_owned')
+    created_by = models.ForeignKey(settings.AUTH_USER_MODEL, null=True, blank=True, on_delete=models.CASCADE)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
 
-    # Shows up in the admin list
     def __str__(self):
         return self.title
+
+
+class Comment(models.Model) :
+    comment = models.TextField(
+        validators=[MinLengthValidator(3, "Comment must be greater than 3 characters")]
+    )
+
+    pic = models.ForeignKey(Pic, on_delete=models.CASCADE)
+    owner = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        if len(self.comment) < 15 : return self.comment
+        return self.comment[:11] + ' ...'
