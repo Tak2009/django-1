bdbd724d727f193f52ce21b4efcb509197c0d660
Tak2009 <50517027+Tak2009@users.noreply.github.com>
2023-02-09 20:48:28 +0000

    tech view updated. need to add some more for oauth to increase rate limit

--- mysite/home/static/api.js
+++ mysite/home/static/api.js
@@ -1,17 +1,46 @@
 // //https://developer.mozilla.org/ja/docs/Web/API/Response#ajax_%E5%91%BC%E3%81%B3%E5%87%BA%E3%81%97
 // //https://blog.share-wis.com/javascript-async-await#toc3
 // //Github: "https://api.github.com/users/Tak2009/repos"
+// //Rate Limit: https://docs.github.com/en/rest/overview/resources-in-the-rest-api?apiVersion=2022-11-28#rate-limiting
 
-const django_1_language = "https://api.github.com/repos/Tak2009/django-1/languages";
+const project_urls = [
+    "https://api.github.com/repos/Tak2009/django-1/languages",
+    "https://api.github.com/repos/Tak2009/raspi-2-motion-detect-sensor-oauth2-tk/languages",
+    "https://api.github.com/repos/Tak2009/raspi-3-moisture-sensor-client-tk/languages",
+    "https://api.github.com/repos/Tak2009/raspi-3-water-pump-server-tk/languages"
+]
 
-const getLang = async (url) => {
-    const response = await fetch(url); // Response オブジェクトを生成する
-    if (response.ok) {
-      const jsonValue = await response.json(); // レスポンスの本体から JSON の値を取得
-      return Promise.resolve(jsonValue);
-    } else {
-      return Promise.reject('***Not Found');
+
+const getProjects = async (url_list) => {
+    const detail_by_project = {}
+    serch_keyword_beg = 'Tak2009/'
+    serch_keyword_end = '/languages'
+    for (url of url_list) {
+        const project_name = url.slice(url.search(serch_keyword_beg) + serch_keyword_beg.length, url.search(serch_keyword_end))
+        const response = await fetch(url); // Response オブジェクトを生成する
+        console.log(url)
+        if (response.ok) {
+          const jsonValue = await response.json(); // レスポンスの本体から JSON の値を取得
+          detail_by_project[project_name] = jsonValue;
+        } else {
+          return Promise.reject('***Not Found');
+        }
     }
-  }
+    return detail_by_project
+}
 
-API = {getLang};
+
+API = {getProjects};
+
+// const getProjects = async (url_list) => {
+//     for (url of url_list) {
+//         const response = await fetch(url); // Response オブジェクトを生成する
+//         console.log(url)
+//         if (response.ok) {
+//           const jsonValue = await response.json(); // レスポンスの本体から JSON の値を取得
+//           return Promise.resolve(jsonValue);
+//         } else {
+//           return Promise.reject('***Not Found');
+//         }
+//     }
+// }
--- mysite/home/static/script_github.js
+++ mysite/home/static/script_github.js
@@ -2,39 +2,59 @@
 // https://qiita.com/7note/items/4d922ebd33dbefe99a89
 background = ['#07FFD2', '#07B0FF', '#0734FF', '#5607FF', '#D207FF', '#FF07B0']
 
-const renderLang = (langs) => {
-    const num_array = Object.values(langs)
+
+const renderProject = (projects) => {
+    const project_list = document.getElementById('project-list');
+    const project_detail = document.getElementById('project-detail');
+    let project_index = 0
+
+    for (project in projects) {
+        let project_li
+        let project_div
+        if (project_index == 0) {
+            project_li = `<li data-target="#carouselExampleIndicators" data-slide-to=${project_index} class="active" />`
+            project_div = 
+                `<div class="carousel-item active">
+                    <p style="margin-bottom: 0; margin-left: 2vw;">Repository Name: ${project}</p>
+                    <div class="container py-4" id=${project_index} style="display: inline-block; min-height: 20vh;"></div>
+                </div>`
+        } else {
+            project_li = `<li data-target="#carouselExampleIndicators" data-slide-to=${project_index} />`
+            project_div = 
+                `<div class="carousel-item">
+                    <p style="margin-bottom: 0; margin-left: 2vw;">Repository Name: ${project}</p>
+                    <div class="container py-4" id=${project_index} style="display: inline-block; min-height: 20vh;"></div>
+                </div>`
+        }
+        project_list.innerHTML += project_li
+        project_detail.innerHTML += project_div
+        renderLanguagesByProject(projects[project], project_index, project)
+        project_index += 1
+    }
+}
+
+
+const renderLanguagesByProject = (project, project_index, project_name) => {
+    const num_array = Object.values(project)
     const sum = num_array.reduce((accumulator, current) => accumulator + current);
-    const lang_list = document.getElementById('skill');
+    const project_index_div = document.getElementById((project_index).toString());
     lang_index = 0
-    for (lang in langs) {
+    for (lang in project) {
+        ratio = (parseFloat(project[lang])/sum * 100).toFixed(1)
         const div = document.createElement("div")
         div.classList.add("progress")
         div.style.backgroundColor = '#f59842'
         div.style.height= '30px'
-
-        console.log(lang_index, lang)
-        ratio = (parseFloat(langs[lang])/sum * 100).toFixed(1)
-        div.innerHTML = 
+        let lang_progress_bar_div = 
             `<div class="progress-bar" role="progressbar" style="width: ${ratio}%; background:${background[lang_index % 6]}" aria-valuenow="${ratio * 100}" aria-valuemin="0" aria-valuemax="100" onMouseOut="this.style.background='${background[lang_index % 6]}';" onMouseOver="this.style.background='#EEF';">
-                <a href="https://github.com/Tak2009/django-1/search?l=${lang.toLowerCase()}" target="_blank" style="color: #f7f5f5" onMouseOut="this.style.color='#f7f5f5';" onMouseOver="this.style.color='#050505';">
+                <a href="https://github.com/Tak2009/${project_name}/search?l=${lang.toLowerCase()}" target="_blank" style="color: #f7f5f5" onMouseOut="this.style.color='#f7f5f5';" onMouseOver="this.style.color='#050505';">
                     ${lang}(${ratio})%
                 </a>
             </div>`
-        lang_list.appendChild(div)
+        div.insertAdjacentHTML("afterbegin", lang_progress_bar_div)
+        project_index_div.appendChild(div)
         lang_index += 1
     }
 }
 
-API.getLang(django_1_language).then(data => renderLang(data)).catch(console.log).finally(console.log("done"));;
-
-// API.getLang(django_1_language)
-// // 成功した場合は取得したリソースのテキストを出力
-// .then(resp) => {
-//   console.log(resp);
-// })
-// // 失敗した場合はエラーを出力
-// .catch((error) => {
-//   console.warn(error);
-// });
-
+API.getProjects(project_urls).then(projects => renderProject(projects)).catch(console.log).finally(console.log("done"));
