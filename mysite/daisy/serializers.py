00e26f59ab69eda0acd6426404b812a95173119c
Tak2009 <50517027+Tak2009@users.noreply.github.com>
2023-02-17 07:46:31 +0000

    changed a read-only field json

--- mysite/daisy/serializers.py
+++ mysite/daisy/serializers.py
@@ -2,6 +2,7 @@ from rest_framework import serializers
 from daisy.models import Pic
 from rest_framework import exceptions
 from django.contrib.auth import authenticate
+from django.conf import settings
 
 # HyperlinkedModelSerializer: https://youtu.be/ruIJdGdgkCw?list=PL1WVjBsN-_NJ4urkLt7iVDocVu_ZQgVzF&t=1313
 # class PicSerializer(serializers.HyperlinkedModelSerializer):
@@ -12,10 +13,21 @@ from django.contrib.auth import authenticate
 
 
 class PicSerializer(serializers.ModelSerializer):
+    created_by = serializers.SerializerMethodField()
+    
     class Meta:
         model = Pic
         fields = ('id','title', 'pic', 'created_by')
         read_only_fields = ('created_by', )
+    
+    # https://stackoverflow.com/questions/56056449/how-to-get-username-of-users-post-in-django-rest-api-instead-of-number
+    # check SerializerMethodField and django.contrib.auth.models.User
+    def get_created_by(self, obj):
+        # print(type(obj.created_by))
+        return {
+            "username": obj.created_by.username,
+            "id": obj.created_by.id
+        }
 
 
 # https://www.youtube.com/watch?v=g8Sz2mF0ENU&list=PL1WVjBsN-_NJ4urkLt7iVDocVu_ZQgVzF&index=8
